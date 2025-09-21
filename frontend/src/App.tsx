8de2f47ffcb36a5f9e89ae2b9c629afe114bcd2d
YunussEmree <senyigityunusemre@gmail.com>
2025-09-21 23:36:49 +0300

    Implemented React Redux

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
+import { store } from './app/store';
 import AppRoutes from './routes';
+import { Provider } from 'react-redux';
+
 
 function App() {
     return (
+      <Provider store={store}>
         <AppRoutes />
+      </Provider>
     );
 }
 
---
+++ frontend/src/features/counter/Counter.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement, incrementByAmount } from "./counterSlice";
+
+interface RootState {
+  counter: {
+    value: number;
+  };
+}
+
+function Counter() {
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={() => dispatch(increment())}>+1</button>
+      <button onClick={() => dispatch(decrement())}>-1</button>
+      <button onClick={() => dispatch(incrementByAmount(5))}>+5</button>
+    </div>
+  );
+}
+
+export default Counter;
